6ecc193b9af3733181de2f2933ae94620f32a545
Margot Paon Garcia <margot.paon@gmail.com>
2020-04-25 00:22:46 -0300

    ADD: Arquivos de configuração para o AutoMapper, Swagger e Identity MOD: Configuração do Swagger e Identity na classe Startup e alteração para WebHost na Program.cs

--- CentralDeErros/ConfigStartup/AutoMapperProfile.cs
+++ CentralDeErros/ConfigStartup/AutoMapperProfile.cs
@@ -10,6 +10,16 @@ namespace CentralDeErros.ConfigStartup
         public AutoMapperProfile()
         {
             CreateMap<User, UserDTO>().ReverseMap();
+            /*
+            CreateMap<Cliente, ClienteDTO>().ReverseMap();
+            CreateMap<Produto, ProdutoJSONDTO>().ReverseMap();
+            CreateMap<Produto, ProdutoDto>().ReverseMap();
+            CreateMap<CompraDTO, Compra>().ReverseMap();
+            CreateMap<PromocaoProduto, PromocaoProdutoDTO>().ReverseMap();
+            CreateMap<Promocao, PromocaoDTO>()
+                .ForMember(dest => dest.PromocaoProdutos, opt => opt.MapFrom(src => src.Produtos))
+                .ReverseMap();
+            CreateMap<ProdutoJSONDTO, Produto>().ReverseMap();*/
 
         }
     }
--- CentralDeErros/Models/CentralErrosContext.cs
+++ CentralDeErros/Models/CentralErrosContext.cs
@@ -17,11 +17,7 @@ namespace CentralDeErros.Models
         {
             //confirmação de configuraão para utilizar com In Memory Database
             if (!optionsBuilder.IsConfigured)
-<<<<<<< HEAD
                 optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=TesteDB; User Id = sa; Password=Jullia@3005;Trusted_Connection=False");
-=======
-                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Codenation;Integrated Security=True;");
->>>>>>> 9f0ee4871e9c6215e1102181e3dd8a910cd9dffc
 
             //optionsBuilder.UseSqlite("Data Source=user.db");
         }
